74bd9905f5ee9ebf5f8c848f7da885f72f86d447
diego2784 <diegoe8427@gmail.com>
2022-12-27 16:32:26 -0300

    Se comenzó con el encabezado

---
+++ proyectoIntegrador_DiegoMarigliano/src/app/components/rsocial/rsocial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rsocial',
+  templateUrl: './rsocial.component.html',
+  styleUrls: ['./rsocial.component.css']
+})
+export class RsocialComponent {
+
+}
